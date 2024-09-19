626dd0ce5e1dab912c82fba4df0f40f421f58502
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-09-19 07:38:16 +0300

    Lord joel

--- leonard/quote.js
+++ leonard/quote.js
@@ -13,14 +13,14 @@ zokou({ nomCom: 'quote', categorie: 'Fun' }, async (dest, zk, commandeOptions) =
         .then((response) => response.json())
         .then(async (quote) => {
           repondre(`╔══════════════════════════╗
-║  joel-md               ║
+║  leonard-md               ║
 ╚══════════════════════════╝
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by joel-it`);
+Powered by leonard it`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
@@ -33,7 +33,7 @@ Powered by joel-it`);
         .then((response) => response.json())
         .then(async (quote) => {
           repondre(`╔══════════════════════════╗
-║   joel-md               ║
+║   leonard md               ║
 ╚══════════════════════════╝
 
 🎬 Anime: ${quote.anime}
