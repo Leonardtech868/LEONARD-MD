b3ac74ddb026ccd08e7cdcc42b0798a43dbc3e62
Leonardtech868 <Leonardtech868@gmail.com>
2024-10-04 10:31:15 +0300

    Update fancy.js
    LEONARD MD

--- leonard/fancy.js
+++ leonard/fancy.js
@@ -1,14 +1,14 @@
 const { zokou } = require("../framework/zokou");
 const fancy = require("../commandes/style");
 
-zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "fancy", categorie: "Fun", reaction: "😎" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
     const id = arg[0]?.match(/\d+/)?.join('');
     const text = arg.slice(1).join(" ");
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 24 leonard md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('Zokou-md', fancy));
+            return await repondre(`\nExemple : ${prefixe} POWERED BY BONIPHACE TECH\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('BONIPHACE-MD', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
